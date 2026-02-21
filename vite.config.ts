f2e339f646a6002cb997f4c56a302c8f1707f232
Nandini Gulhane <nandinigulhane8@gmail.com>
2026-02-21 23:22:47 +0530

    fix: added base url in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Gaurav_Hote_Portfolio/",
   server: {
+    open: true,
     host: "::",
     port: 8080,
   },
