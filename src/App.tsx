f5b28ef15a19235a4e09495c657830f96a839fb6
Gaurav Hote <gauravhote13@gmail.com>
2026-03-06 22:30:07 +0530

    base config added.

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/Gaurav_Hote_Portfolio/">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Gaurav_Hote_Portfolio/",
+  base: "/",
   server: {
     open: true,
     port: 8080,
